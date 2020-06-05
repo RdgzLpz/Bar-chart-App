43aad40850d068da95cb51e8c32d67ac8e2439f0
Oscar Rodriguez <varios_varios@outlook.com>
2020-06-05 18:04:15 -0430

    This is my second commit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-let yMargin = 40;
-let width = 800;
-let height= 400;
-let barWidth = width / 275;
+//Oscar Rodriguez
+let w = 800;
+let h= 400;
+let rectWidth = w / 275;
+
 let tooltip = d3.select(".visHolder")
                 .append("div")
                 .attr("id", "tooltip")
-                .style("opacity", 0);
+                .style("opacity", 0); 
 
 let overlay = d3.select(".visHolder")
                 .append("div")
@@ -14,32 +15,35 @@ let overlay = d3.select(".visHolder")
 
 const svg = d3.select(".visHolder")
               .append("svg")
-              .attr("width", width + 100)
-              .attr("height", height + 60);
+              .attr("width", w + 100)
+              .attr("height", h + 60);
 
 fetch("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json")
     .then(res => res.json())
     .then(data => {
-        svg.append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("x", -200)
-          .attr("y", 80)
-          .text("Gross Domestic Product");
-
-        svg.append("text")       
-           .attr("x", width / 2 + 120)  
-           .attr("y", height + 50)
-           .attr("class", "info")
-           .text("More Information: http://www.bea.gov/national/pdf/nipaguid.pdf");  
 
         let years = data.data.map(i => {
-            let quarter;
+            let Q;
             let temp =  i[0].substring(5, 7);
+              switch (temp) {
+                     case "01":
+                            Q = "Q1";
+                            break;
+                     case "04":
+                            Q = "Q2";       
+                            break;
+                     case "07":
+                            Q = "Q3";
+                            break;       
+                     default:
+                            Q = "Q4";
+                            break;
+              }
 
-            if (temp === "01") quarter = "Q1";
-            else if (temp === "04") quarter = "Q2";
-            else if (temp === "07") quarter = "Q3";
-            else if (temp === "10") quarter = "Q4";
+       //      if (temp === "01") quarter = "Q1";
+       //      else if (temp === "04") quarter = "Q2";
+       //      else if (temp === "07") quarter = "Q3";
+       //      else if (temp === "10") quarter = "Q4";
 
             return i[0].substring(0, 4) + " " + quarter;
         });
@@ -51,39 +55,39 @@ fetch("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/maste
 
         const xScale = d3.scaleTime()
                          .domain([d3.min(yearsDate), xMax])
-                         .range([0, width]);
+                         .range([0, w]);
 
         const xAxis = d3.axisBottom()
                         .scale(xScale);
                         
-        const xAxisGroup =  svg.append("g")
-                               .attr("transform", "translate(60, 400)")        
-                               .attr("id", "x-axis") 
-                               .call(xAxis);       
+        svg.append("g")
+           .attr("transform", "translate(60, 400)")        
+           .attr("id", "x-axis") 
+           .call(xAxis);       
 
-        const GDP = data.data.map(i => i[1]);
+        const billions = data.data.map(i => i[1]);
 
         let scaledGDP = [];
 
-        let gdpMin = d3.min(GDP);
-        let gdpMax = d3.max(GDP);
+        let gdpMin = d3.min(billions);
+        let gdpMax = d3.max(billions);
 
         const linearScale = d3.scaleLinear()
                               .domain([0, gdpMax])
-                              .range([0, height]);
+                              .range([0, h]);
 
-        scaledGDP = GDP.map(i => linearScale(i));
+        scaledGDP = billions.map(i => linearScale(i));
         
         const yAxisScale = d3.scaleLinear()
                              .domain([0, gdpMax])
-                             .range([height, 0]);
+                             .range([h, 0]);
 
         const yAxis = d3.axisLeft(yAxisScale);     
         
-        const yAxisGroup = svg.append("g") 
-                              .attr("transform", "translate(60, 0)")
-                              .attr("id", "y-axis")
-                              .call(yAxis);
+        svg.append("g") 
+           .attr("transform", "translate(60, 0)")
+           .attr("id", "y-axis")
+           .call(yAxis);
 
         d3.select("svg")
           .selectAll("rect")
@@ -94,8 +98,8 @@ fetch("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/maste
           .attr("data-gdp", (d, i) => data.data[i][1]) 
           .attr("class", "bar") 
           .attr("x", (d, i) => xScale(yearsDate[i])) 
-          .attr("y", d => height - d) 
-          .attr("width", barWidth) 
+          .attr("y", d => h - d) 
+          .attr("width", rectWidth) 
           .attr("height", d => d)
           .attr("transform", "translate(60, 0)")
           .style("fill", "blue")
@@ -103,19 +107,19 @@ fetch("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/maste
               overlay.transition()
                      .duration(100)
                      .style("height", d + "px")
-                     .style("width", barWidth + "px")
+                     .style("width", rectWidth + "px")
                      .style("opacity", .9)
-                     .style("left", (i * barWidth) + "px")
-                     .style("top", height - d + "px")
+                     .style("left", (i * rectWidth) + "px")
+                     .style("top", h - d + "px")
                      .style("transform", "translateX(60px)");
               tooltip.transition()       
                      .duration(200)
                      .style("opacity", .9);
                      
-              tooltip.html(years[i] + "<br>" + "$" + GDP[i].toFixed(1).replace(/(\d)(?=(\d{3})+\.)/g, "$1,") + "Billion")       
+              tooltip.html(years[i] + "<br>" + "$" + billions[i].toFixed(1).replace(/(\d)(?=(\d{3})+\.)/g, "$1,") + "Billion")       
                      .attr("data-date", data.data[i][0])
-                     .style("left", (i * barWidth) + 80 + "px")
-                     .style("top", height - 100 + "px")
+                     .style("left", (i * rectWidth) + 80 + "px")
+                     .style("top", h - 100 + "px")
           })
           
           .on("mouseout", d => {
@@ -126,6 +130,19 @@ fetch("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/maste
                    .duration(200)
                    .style("opacity", 0);
           })
+
+          //appending the text
+          svg.append("text")
+          .attr("transform", "rotate(270)")
+          .attr("x", -200)
+          .attr("y", 80)
+          .text("USA Gross Domestic Product");
+
+          svg.append("text")       
+           .attr("x", w / 2 + 120)  
+           .attr("y", h + 50)
+           .attr("class", "moreInformation")
+           .text("More Information? visit: http://www.bea.gov/national/pdf/nipaguid.pdf");  
 
 
     })           
